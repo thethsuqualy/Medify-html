2ff6262320baaa8ec14e71420435fc4f32977b17
thethsuqualy <thethsukhin.qualymm@gmail.com>
2025-06-23 16:53:09 +0630

    add

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,5 +1,14 @@
 // Your JavaScript code goes here
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+ document.querySelectorAll('.lower-carea-h4').forEach(function(el) {
+  el.addEventListener('click', function() {
+    document.querySelectorAll('.lower-carea-h4').forEach(function(item) {
+      item.classList.remove('active');
+    });
+    el.classList.add('active');
+  });
+});
+});
 
 // Your jQuery code goes here
 $(function () {
