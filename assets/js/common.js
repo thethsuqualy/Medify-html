c0bab2a6acd2238d365e36ca5ca9386614bc3420
TinZarPhyoWaiQualy <tinzarphyowai.qualymm@gmail.com>
2025-07-02 14:11:07 +0630

    update blog2column

--- assets/js/common.js
+++ assets/js/common.js
@@ -182,8 +182,8 @@ $(function () {
       dots: false,
       infinite: true,
       arrows: true,
-      prevArrow: '.next',
-      nextArrow: '.prev',
+      prevArrow: ".next",
+      nextArrow: ".prev",
       autoplay: false,
       autoplaySpeed: 3000,
     });
@@ -206,53 +206,33 @@ $(function () {
     });
   });
   //counter setup end faq
+
+  //blog2column slider start
+
   $(".wlslider").each(function () {
-      const $slider = $(this);
-      const slideCount = $slider.children().length;
+    console.log("jQuery loaded:", typeof $);
+    const $slider = $(this);
+    const slideCount = $slider.children().length;
 
-      if (slideCount < 2) return;
+    if (slideCount < 2) return;
 
-      $slider.slick({
-        autoplay: false,
-        arrows: true,
-        dots: false,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        pauseOnHover: false,
-        adaptiveHeight: true,
-        speed: 1500,   
-        fade: true,              
-        cssEase: 'ease-in-out',   
-        prevArrow: $('.custom-prev'), 
-        nextArrow: $('.custom-next'),
-      });
+    $slider.slick({
+      autoplay: false,
+      arrows: true,
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      pauseOnHover: false,
+      adaptiveHeight: true,
+      speed: 1500,
+      fade: true,
+      cssEase: "ease-in-out",
+      prevArrow: $(".custom-prev"),
+      nextArrow: $(".custom-next"),
     });
-  
-  //blog2column slider image start
-  //  $(function () {
-  //   $(".wlslider").each(function () {
-  //     const $slider = $(this);
-  //     const slideCount = $slider.children().length;
+  });
 
-  //     if (slideCount < 2) return;
-
-  //     $slider.slick({
-  //       autoplay: false,
-  //       arrows: true,
-  //       dots: false,
-  //       infinite: true,
-  //       slidesToShow: 1,
-  //       slidesToScroll: 1,
-  //       pauseOnHover: false,
-  //       adaptiveHeight: true,
-  //       speed: 1500,   
-  //       fade: true,              
-  //       cssEase: 'ease-in-out',   
-  //       prevArrow: $('.custom-prev'), 
-  //       nextArrow: $('.custom-next'),
-  //     });
-  //   });
-  // });
-  //blog2column slider image end
+  //blog2column slider end
 });
+
